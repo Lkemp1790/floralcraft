d4761b27ae5c3ca60b5430de6d157a78b6b92f57
Liam Kemp <lkemp1790@gmail.com>
2025-10-23 13:27:19 +0100

    Update Footer component with new links and refined styling for enhanced user experience

---
+++ client/src/lib/types.ts
@@ -0,0 +1,17 @@
+export type Product = {
+    id: number
+    name: string
+    price: number
+    image: string
+    categories: string[]
+    flowerTypes: string[]
+    description: string
+    stock: number
+    isFeatured: boolean
+    isNew: boolean
+    isBestSeller: boolean
+    isSale: boolean
+    isTrending: boolean
+    isPopular: boolean
+    isRecommended: boolean
+}
